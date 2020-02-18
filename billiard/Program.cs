8cbed99d425eac536fec28210b2b1bb536433daf
Hoffmann.László <Hoffmann.Lszl@C102-02>
2020-02-18 14:06:06 +0100

    Hibajavítás

--- billiard/Program.cs
+++ billiard/Program.cs
@@ -12,16 +12,29 @@ namespace billiard
         {
             List<int> golyók = new List<int>();
             Kezdés(golyók);
-            int leeső=LeesőSoroló(golyók);
-            Kiíró(golyók);
-            
+            int leeső;
+            do
+            {
+                leeső=LeesőSoroló(golyók);
+                Kiíró(golyók);
+                Console.WriteLine(leeső);
+            } while (leeső != 8);
+            if (leeső==8 && golyók.Count==0)
+            {
+                Console.WriteLine("Nyertél!");
+            }
+            else
+            {
+                Console.WriteLine("Vesztettél!");
+            }
+            Console.ReadKey();
         }
 
         static void Kiíró(List<int> golyók)
         {
             foreach (int item in golyók)
             {
-                Console.WriteLine($"{item}");
+                Console.Write($"{item} ");
             }
         }
 
@@ -29,7 +42,7 @@ namespace billiard
         {
             Random gép = new Random();
             int szám= gép.Next(golyók.Count);
-            golyók.Remove(szám);
+            golyók.Remove(golyók[szám]);
             return golyók[szám];
         }
         
